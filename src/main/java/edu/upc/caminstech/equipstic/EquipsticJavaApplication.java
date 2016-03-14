19040f43ae08a7c87f32a987b2afdef421828086
Angel Aguilera <angel.aguilera@gmail.com>
2016-03-14 14:38:43 +0100

    Creat l'esquelet amb Spring Initializr.

---
+++ src/main/java/edu/upc/caminstech/equipstic/EquipsticJavaApplication.java
@@ -0,0 +1,12 @@
+package edu.upc.caminstech.equipstic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipsticJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipsticJavaApplication.class, args);
+	}
+}
---
+++ src/test/java/edu/upc/caminstech/equipstic/EquipsticJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.upc.caminstech.equipstic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = EquipsticJavaApplication.class)
+public class EquipsticJavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
