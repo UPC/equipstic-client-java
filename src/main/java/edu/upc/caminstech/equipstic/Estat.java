b85a6caaf41e44189e73acdc74bb5de11b2a2a18
Angel Aguilera <angel.aguilera@gmail.com>
2016-04-08 12:51:45 +0200

    Added class 'Estat'

---
+++ src/main/java/edu/upc/caminstech/equipstic/Estat.java
@@ -0,0 +1,53 @@
+package edu.upc.caminstech.equipstic;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Estat {
+
+    public enum TipusEstat {
+        VALIDACIO, INFRAESTRUCTURA;
+    }
+
+    private final long idEstat;
+    private final String nom;
+    private final TipusEstat tipusEstat;
+    private final String codi;
+    private final boolean requereixValidacio;
+
+    @JsonCreator
+    public Estat( //
+            @JsonProperty("idEstat") long idEstat, //
+            @JsonProperty("nom") String nom, //
+            @JsonProperty("tipusEstat") TipusEstat tipusEstat, //
+            @JsonProperty("codi") String codi, //
+            @JsonProperty("requereixValidacio") boolean requereixValidacio) {
+
+        this.idEstat = idEstat;
+        this.nom = nom;
+        this.tipusEstat = tipusEstat;
+        this.codi = codi;
+        this.requereixValidacio = requereixValidacio;
+    }
+
+    public long getIdEstat() {
+        return idEstat;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public TipusEstat getTipusEstat() {
+        return tipusEstat;
+    }
+
+    public String getCodi() {
+        return codi;
+    }
+
+    public boolean isRequereixValidacio() {
+        return requereixValidacio;
+    }
+
+}
