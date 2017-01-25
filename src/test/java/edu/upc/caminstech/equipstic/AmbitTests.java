4f9d105d93b38cff7db680983bc1a6f051c76144
Angel Aguilera <angel.aguilera@gmail.com>
2017-01-25 11:31:41 +0100

    added: serialization tests for Ambit

--- src/test/java/edu/upc/caminstech/equipstic/AmbitTests.java
+++ src/test/java/edu/upc/caminstech/equipstic/AmbitTests.java
@@ -2,14 +2,21 @@ package edu.upc.caminstech.equipstic;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.util.StringUtils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class AmbitTests {
 
     private Ambit ambit1;
     private Categoria categoriaInfraestructura;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     @Before
     public void setUp() throws Exception {
@@ -54,6 +61,31 @@ public class AmbitTests {
     public void testHashCode() {
         Ambit copy = new Ambit(1, "Ambit 1", "CODI_AMBIT_1", categoriaInfraestructura);
         assertEquals(ambit1.hashCode(), copy.hashCode());
+    }
+
+    @Test
+    public void testSerialize() throws JsonProcessingException {
+        String actual = objectMapper.writeValueAsString(ambit1);
+        Map<String, Object> obj = new LinkedHashMap<>();
+        obj.put("idAmbit", ambit1.getIdAmbit());
+        obj.put("nom", ambit1.getNom());
+        obj.put("codi", ambit1.getCodi());
+        obj.put("categoriaInfraestructura", ambit1.getCategoriaInfraestructura());
+        String expected = objectMapper.writeValueAsString(obj);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSerializeNullAttributes() throws JsonProcessingException {
+        long id = 23L;
+        Ambit ambit = new Ambit(id);
+        String actual = objectMapper.writeValueAsString(ambit);
+        Map<String, Object> obj = new LinkedHashMap<>();
+        obj.put("idAmbit", id);
+        String expected = objectMapper.writeValueAsString(obj);
+
+        assertEquals(expected, actual);
     }
 
 }
