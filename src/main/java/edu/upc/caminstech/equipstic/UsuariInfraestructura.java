48ea522df59199896ae6a2253d275038dba6062e
Angel Aguilera <angel.aguilera@gmail.com>
2017-02-13 11:49:07 +0100

    doc: added javadoc comment

--- src/main/java/edu/upc/caminstech/equipstic/UsuariInfraestructura.java
+++ src/main/java/edu/upc/caminstech/equipstic/UsuariInfraestructura.java
@@ -7,6 +7,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Usuari habitual d'un equip.
+ */
 @JsonInclude(Include.NON_NULL)
 public class UsuariInfraestructura {
 
