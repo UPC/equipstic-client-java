bfb8abdc82e6342be9359c9b0d8d809aa28a0005
Angel Aguilera <angel.aguilera@gmail.com>
2017-04-05 18:55:17 +0200

    change: updated URL for API v2

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Modifiqueu les variables amb els valors oportuns
-export EQUIPSTIC_SOA_URL="https://bus-soades.upc.edu/EquipsTIC/APIv1"
+export EQUIPSTIC_SOA_URL="https://bus-soades.upc.edu/EquipsTIC/APIv2"
 export EQUIPSTIC_SOA_USERNAME="my_soa_username"
 export EQUIPSTIC_SOA_PASSWORD="my_soa_password"
 
