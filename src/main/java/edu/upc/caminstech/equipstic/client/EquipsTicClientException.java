2a19b75f7f7787f5a43c7c483ff4931381595a12
Angel Aguilera <angel.aguilera@gmail.com>
2017-04-28 11:34:04 +0200

    change: encapsulate HttpClientErrorException
    Now EquipsTicClientException accepts an HttpClientErrorException to
    support error handling when the server does not return a valid ResponseEntity
    but throws an exception instead.

--- src/main/java/edu/upc/caminstech/equipstic/client/EquipsTicClientException.java
+++ src/main/java/edu/upc/caminstech/equipstic/client/EquipsTicClientException.java
@@ -1,8 +1,12 @@
 package edu.upc.caminstech.equipstic.client;
 
+import java.util.Optional;
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.client.HttpClientErrorException;
 
 /**
  * Excepció del Client que exposa la resposta retornada pel servidor.
@@ -14,46 +18,70 @@ public class EquipsTicClientException extends RuntimeException {
 
     private static final long serialVersionUID = 5559008561162893859L;
 
-    private final ResponseEntity<?> entity;
     private final Response<?> response;
+    private final HttpStatus status;
+    private final HttpHeaders headers;
 
     public EquipsTicClientException(ResponseEntity<?> entity, Response<?> response) {
         super();
-        this.entity = entity;
+        Assert.notNull(entity, "Entity can not be null");
         this.response = response;
+        this.status = entity.getStatusCode();
+        this.headers = entity.getHeaders();
     }
 
     public EquipsTicClientException(ResponseEntity<?> entity, Response<?> response, String message) {
         super(message);
-        this.entity = entity;
+        Assert.notNull(entity, "Entity can not be null");
         this.response = response;
+        this.status = entity.getStatusCode();
+        this.headers = entity.getHeaders();
     }
 
     public EquipsTicClientException(ResponseEntity<?> entity, Response<?> response, Throwable cause) {
         super(cause);
-        this.entity = entity;
+        Assert.notNull(entity, "Entity can not be null");
         this.response = response;
+        this.status = entity.getStatusCode();
+        this.headers = entity.getHeaders();
     }
 
     public EquipsTicClientException(ResponseEntity<?> entity, Response<?> response, String message, Throwable cause) {
         super(message, cause);
-        this.entity = entity;
+        Assert.notNull(entity, "Entity can not be null");
         this.response = response;
+        this.status = entity.getStatusCode();
+        this.headers = entity.getHeaders();
+    }
+
+    public EquipsTicClientException(HttpClientErrorException e) {
+        super(e);
+        this.response = null;
+        this.status = e.getStatusCode();
+        this.headers = e.getResponseHeaders();
     }
 
     /**
      * Retorna la resposta del servidor.
      */
-    public Response<?> getResponse() {
-        return response;
+    public Optional<Response<?>> getResponse() {
+        return Optional.ofNullable(response);
     }
 
-    public HttpStatus getStatusCode() {
-        return entity != null ? entity.getStatusCode() : null;
+    /**
+     * Retorna l'status HTTP de la resposta del servidor.
+     * 
+     * @return
+     */
+    public HttpStatus getStatus() {
+        return status;
     }
 
+    /**
+     * Retorna els headers de la resposta del servidor.
+     */
     public HttpHeaders getHeaders() {
-        return entity != null ? entity.getHeaders() : null;
+        return headers;
     }
 
 }
