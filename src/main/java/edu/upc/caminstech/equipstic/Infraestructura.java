2154f7a5c90332a671a597cb903a63ff8688332e
Angel Aguilera <angel.aguilera@gmail.com>
2017-10-02 12:41:22 +0200

    fix: incorrect field name "numeroAds"
    The server returns a JSON string with invalid field name "numeroAds"
    (pluralized), instead of "numeroAd". This is a temporary workaround
    until the bug in the API is fixed.

--- src/main/java/edu/upc/caminstech/equipstic/Infraestructura.java
+++ src/main/java/edu/upc/caminstech/equipstic/Infraestructura.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(Include.NON_NULL)
 public class Infraestructura implements Comparable<Infraestructura> {
@@ -213,6 +214,18 @@ public class Infraestructura implements Comparable<Infraestructura> {
     }
 
     public void setNumeroAd(String numeroAd) {
+        this.numeroAd = numeroAd;
+    }
+
+    /**
+     * Workaround per solventar problema en la deserialització.
+     * 
+     * FIXME: Aquest mètode es pot eliminar quan arreglin el bug pel qual la
+     * operació "GET /infraestructura/{id}/detalls" retorna el camp "numeroAd"
+     * amb un nom erroni (incloent una "s" final).
+     */
+    @JsonProperty("numeroAds")
+    public void setNumeroAds(String numeroAd) {
         this.numeroAd = numeroAd;
     }
 
