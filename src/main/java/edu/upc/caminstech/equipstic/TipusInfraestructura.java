c4157493c677d665635eae1ac86e2892012c95ba
Angel Aguilera <angel.aguilera@gmail.com>
2020-05-26 12:36:15 +0200

    Add methods to check for required attributes
    Methods calModelCpu, calNumeroCpus, etc. are implemented inside
    TipusInfraestructura for better decoupling.

--- src/main/java/edu/upc/caminstech/equipstic/TipusInfraestructura.java
+++ src/main/java/edu/upc/caminstech/equipstic/TipusInfraestructura.java
@@ -1,14 +1,44 @@
 package edu.upc.caminstech.equipstic;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 import org.apache.commons.lang3.builder.CompareToBuilder;
+import org.springframework.lang.Nullable;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Tipus d'infraestructura.
+ * <p>
+ * Cada {@code TipusInfraestructura} té associada una {@link Categoria}. Cada
+ * combinació de tipus/categoria té uns atributs obligatoris o uns altres a la
+ * API d'EquipsTIC (per exemple model i nombre de CPU, capacitat dels discs,
+ * etc.).
+ * <p>
+ * Consulteu la documentació de <a href=
+ * "https://equipstic.upc.edu/equipstic-api-v2/swagger-ui.html#!/infraestructura/addUsingPOST">POST
+ * /infraestructura</a> per veure quins camps són obligatoris per
+ * tipus/categoria.
+ */
 @JsonInclude(Include.NON_NULL)
 public class TipusInfraestructura implements Comparable<TipusInfraestructura> {
+
+    /*
+     * Algunes constants que es fan servir a funcions auxiliars (no estan tots
+     * els tipus i categories existents)
+     */
+
+    private static final String EQUIP_COMPUTACIO = "EQUIP_COMPUTACIO";
+    private static final String EQUIP_TREBALL = "EQUIP_TREBALL";
+    private static final String DISPOSITIU_EMMAGATZEMAMENT = "DISPOSITIU_EMMAGATZEMAMENT";
+
+    private static final String TIPUS_ESTACIO_DE_TREBALL = "ESTACIO_TREBALL";
+    private static final String TIPUS_NETBOOK = "'NETBOOK";
+    private static final String TIPUS_PORTATIL = "PORTATIL";
 
     private final long idTipus;
     private final String nom;
@@ -54,6 +84,35 @@ public class TipusInfraestructura implements Comparable<TipusInfraestructura> {
         return requereixCampsExtra;
     }
 
+    public boolean calModelCpu() {
+        String codiTipus = this.getCodi();
+        return Arrays.asList(DISPOSITIU_EMMAGATZEMAMENT, EQUIP_COMPUTACIO).contains(getCodiCategoria())
+                || (EQUIP_TREBALL.equals(getCodiCategoria())
+                        && Arrays.asList(TIPUS_ESTACIO_DE_TREBALL, TIPUS_NETBOOK, TIPUS_PORTATIL).contains(codiTipus));
+    }
+
+    public boolean calNumeroCpus() {
+        return calModelCpu();
+    }
+
+    public boolean calDiscs() {
+        return calModelCpu();
+    }
+
+    public boolean calCapacitatTotalGb() {
+        return Arrays.asList(DISPOSITIU_EMMAGATZEMAMENT, EQUIP_COMPUTACIO).contains(getCodiCategoria());
+    }
+
+    public boolean calUsuari() {
+        return EQUIP_TREBALL.equals(getCodiCategoria());
+    }
+
+    public boolean calSistemaOperatiu() {
+        String codiTipus = this.getCodi();
+        return EQUIP_COMPUTACIO.equals(getCodiCategoria()) || (EQUIP_TREBALL.equals(getCodiCategoria())
+                && Arrays.asList(TIPUS_ESTACIO_DE_TREBALL, TIPUS_NETBOOK, TIPUS_PORTATIL).contains(codiTipus));
+    }
+
     @Override
     public String toString() {
         return String.format(
@@ -82,7 +141,17 @@ public class TipusInfraestructura implements Comparable<TipusInfraestructura> {
         if (t == this) {
             return 0;
         }
-        return new CompareToBuilder().append(this.categoriaInfraestructura, t.categoriaInfraestructura)
-                .append(this.nom, t.nom).append(this.idTipus, t.idTipus).toComparison();
+        return new CompareToBuilder() //
+                .append(this.categoriaInfraestructura, t.categoriaInfraestructura) //
+                .append(this.nom, t.nom) //
+                .append(this.idTipus, t.idTipus) //
+                .toComparison();
+    }
+
+    @Nullable
+    private String getCodiCategoria() {
+        return Optional.ofNullable(this.getCategoriaInfraestructura()) //
+                .map(Categoria::getCodi) //
+                .orElse(null);
     }
 }
